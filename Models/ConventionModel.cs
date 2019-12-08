19a53b132884d192a0d2d69084cc1de1d800a923
Ali Tahir <mo.alitahir9@gmail.com>
2019-12-08 05:42:16 +0100

    Ajoutez des fichiers projet.

---
+++ Models/ConventionModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ConventionStage.Models
+{
+    public class ConventionModel
+    {
+        [Display(Name = "ID Etudiant")]
+        //[Range(10000, 99999, ErrorMessage = "You need to enter a valid student ID")]
+        public int IdEtudiant { get; set; }
+
+        [Display(Name = "Nom de l'entreprise")]
+        [Required(ErrorMessage = "Vous devez renseigner un nom d'entreprise")]
+        public string NomEntreprise { get; set; }
+
+        [Display(Name = "Ville")]
+        [Required(ErrorMessage = "Vous devez renseigner le lieu du stage")]
+        public string Ville { get; set; }
+
+        [Display(Name = "Date de début")]
+        [Required(ErrorMessage = "Vous devez renseigner une date de debut")]
+        public DateTime DateDebut { get; set; }
+
+        [Display(Name = "Date de fin")]
+        [Required(ErrorMessage = "Vous devez renseigner une date de fin")]
+        public DateTime DateFin { get; set; }
+
+        public string Etat { get; set; }
+    }
+}
